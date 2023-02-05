0683061976ea820b2d17e609032f7dd2d8ee8351
VincentRPS <vincentbusiness55@gmail.com>
2023-02-05 11:27:43 +0800

    chore: change runtime

--- source/gateway/config/runtime.exs
+++ source/gateway/config/runtime.exs
@@ -4,8 +4,10 @@ Dotenv.load()
 
 config :database, Derailed.Database.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("PG_URI"),
   pool_size: 12,
+  hostname: System.get_env("PG_HOST", "localhost"),
+  port: String.to_integer(System.get_env("PG_PORT", "5432")),
   database: System.get_env("PG_DATABASE", "postgres"),
   username: System.get_env("PG_USERNAME", "postgres"),
-  password: System.get_env("PG_PASSWORD", "mysecretpassword")
+  password: System.get_env("PG_PASSWORD", "mysecretpassword"),
+  show_sensitive_data_on_connection_error: true
