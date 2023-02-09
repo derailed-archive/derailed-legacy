c7bf59e412a678a93fd28763eb6c87637a6425fb
VincentRPS <vincentbusiness55@gmail.com>
2023-02-09 22:59:42 +0800

    chore: change intro

--- source/frontend/components/web-components/Intro.tsx
+++ source/frontend/components/web-components/Intro.tsx
@@ -4,19 +4,14 @@ import React from 'react';
 export default function Intro() {
     return (
         <div className="font-dino text-center text-white">
-			<div id="ditch" className="pt-9 text-4xl font-semibold">
-				It's time to ditch Discord and Guilded.
-				<br />
-				Derailed is here.
+			<div id="plat" className="pt-9 text-4xl font-semibold">
+				Free & Open Platform for Gamers.
 			</div>
 			<div
-				id="ditch-pitch"
+				id="pitch"
 				className="pt-3 text-lg font-normal max-w-2xl m-auto"
 			>
-				Stop wasting your time on Discord's garbage decisions, and stop being slowed down by
-				Guilded.
-				<br />
-				Simplify your setup, enhance your Guilds.
+				An open text & voice chat for <b>Guilds</b>. Enhanced Gaming integrations, better activity support, and more coming soon...
 			</div>
         </div>
     )
