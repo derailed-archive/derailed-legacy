c387650653c0c9cbfd8602fcb39d6cda5b5c5d42
VincentRPS <vincentbusiness55@gmail.com>
2023-03-02 21:27:46 +0800

    refactor: lowercase component file names

--- apps/derailed-app/src/main.tsx
+++ apps/derailed-app/src/main.tsx
@@ -2,11 +2,11 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Loading from './Loading'
+import Loading from './loading'
 
 
-const App = React.lazy(() => import('./App'))
-const Logout = React.lazy(() => import('./Logout'))
+const App = React.lazy(() => import('./app'))
+const Logout = React.lazy(() => import('./logout'))
 const Login = React.lazy(() => import('@derailed/accounts/login'))
 const Register = React.lazy(() => import('@derailed/accounts/register'))
 
